4a55dcb140774384b659834934f30c8377e94e86
pekalkan <enesfurkan.pekalkan@std.bogazici.edu.tr>
2025-11-08 21:53:37 +0300

    back_to_dot

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-import pygame
-
-# ... other code ...
-
-class Snake:
-    # ... other methods ...
-
-    def draw(self, surf: pygame.Surface, camx: float, camy: float) -> None:
-        # Render a continuous tube: thick polyline + rounded caps
-        path = [world_to_screen(p[0], p[1], camx, camy) for p in self.points]
-        if len(path) >= 2:
-            w = max(1, int(self.thickness * 2))
-            # Thick body
-            pygame.draw.lines(surf, SNAKE_BODY, False, path, w)
-            # Anti-aliased outline to soften edges
-            pygame.draw.aalines(surf, SNAKE_BODY, False, path)
-            # Tail rounded cap
-            pygame.draw.circle(surf, SNAKE_BODY, path[-1], self.thickness)
-
-        # Head rounded cap + highlight
-        hx, hy = world_to_screen(self.x, self.y, camx, camy)
-        pygame.draw.circle(surf, SNAKE_HEAD, (hx, hy), self.thickness + 2)
-
-# ... rest of the code ...
 """snakeater – continuous tube snake, infinite world, food + boost"""
 
 import math
@@ -202,18 +178,10 @@ class Snake:
         self._self_cut_if_crossed()
 
     def draw(self, surf: pygame.Surface, camx: float, camy: float) -> None:
-        # Render a continuous tube: thick polyline + rounded caps
-        path = [world_to_screen(p[0], p[1], camx, camy) for p in self.points]
-        if len(path) >= 2:
-            w = max(1, int(self.thickness * 2))
-            # Thick body
-            pygame.draw.lines(surf, SNAKE_BODY, False, path, w)
-            # Anti-aliased outline to soften edges
-            pygame.draw.aalines(surf, SNAKE_BODY, False, path)
-            # Tail rounded cap
-            pygame.draw.circle(surf, SNAKE_BODY, path[-1], self.thickness)
-
-        # Head rounded cap + highlight
+        # Classic dot-trail rendering (smooth thanks to _push_head_samples)
+        for p in self.points:
+            sx, sy = world_to_screen(p[0], p[1], camx, camy)
+            pygame.draw.circle(surf, SNAKE_BODY, (sx, sy), self.thickness)
         hx, hy = world_to_screen(self.x, self.y, camx, camy)
         pygame.draw.circle(surf, SNAKE_HEAD, (hx, hy), self.thickness + 2)
 
